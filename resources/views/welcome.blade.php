4b00905f9b338b622ae386891352e483e6f52907
unknown <fadlan123098@gmail.com>
2024-07-16 22:35:37 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('template.main')
+@section('content')
+
+<body>
+    
+</body>
+
+<figure class="text-start">
+  <blockquote class="blockquote">
+    <p>SELAMAT DATANG</p>
+  </blockquote>
+  <figcaption class="blockquote-footer">
+    Muhammad <cite title="Source Title">Fadlan</cite>
+  </figcaption>
+</figure>
+
+@endsection
